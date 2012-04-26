96dc089f5e03281069fe202b1258654fb6cbd1e1
Robert J. Macomber <robert.macomber@socrata.com>
2012-04-26 15:42:43 -0700

    FEATURE: implicit conversions from Function to java.io.File[name]Filter

--- src/main/scala/com/socrata/util/implicits/implicits.scala
+++ src/main/scala/com/socrata/util/implicits/implicits.scala
@@ -1,9 +1,16 @@
 package com.socrata.util
 
 import `implicits-impl`._
+import java.io.{FileFilter, File, FilenameFilter}
 
 package object implicits {
   implicit def enhancefile(f: java.io.File) = new EnhancedFile(f)
   implicit def comparatorToOrdering[T](c: java.util.Comparator[T]): Ordering[T] = new ComparatorOrdering(c)
   implicit def comparableToOrdering[T <: Comparable[T]]: Ordering[T] = ComparableOrderingImpl.asInstanceOf[Ordering[T]]
+  implicit def f2filefilter(f: File => Boolean) = new FileFilter {
+    def accept(pathname: File) = f(pathname)
+  }
+  implicit def f2filenamefilter(f: (File, String) => Boolean) = new FilenameFilter {
+    def accept(dir: File, name: String) = f(dir, name)
+  }
 }
