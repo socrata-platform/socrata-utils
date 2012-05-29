7defdf83c8ec8516a019515f7169c4744e6c8849
Robert J. Macomber <robert.macomber@socrata.com>
2012-05-29 11:21:31 -0700

    BUILD: Fix test in the 2.8 crossbuild

--- src/test/scala/com/socrata/util/hashing/MurmurHash3Test.scala
+++ src/test/scala/com/socrata/util/hashing/MurmurHash3Test.scala
@@ -24,6 +24,31 @@ class MurmurHash3Test extends FunSuite with PropertyChecks with MustMatchers {
   }
 
   test("Hashing strings is equivalent to hashing their big-endian UTF-16 representations") {
+    // ArbitraryValidString is from rojoma-json.  We need it because
+    // in this test we're converting a string to an array of bytes
+    // using a codec.  This only preserves complete information if the
+    // string is well-formed.  ScalaCheck 1.9's String instance will
+    // select only non-surrogate characters but ScalaCheck 1.8's will
+    // choose any random Char at all, which will break the test.  So
+    // if we'll be providing an Arbitrary instance, we might as well
+    // provide one that is willing to cover the full range of Unicode.
+    import org.scalacheck.{Arbitrary, Gen}
+    implicit val ArbitraryValidString = Arbitrary[String] {
+      val lowSurrogate = Gen.choose(Character.MIN_LOW_SURROGATE, Character.MAX_LOW_SURROGATE).map(_.toChar)
+
+      val notLowSurrogate = Gen.frequency(
+        (Character.MIN_LOW_SURROGATE - Char.MinValue, Gen.choose(Char.MinValue, Character.MIN_LOW_SURROGATE - 1)),
+        (Char.MaxValue - Character.MAX_LOW_SURROGATE, Gen.choose(Character.MAX_LOW_SURROGATE + 1, Char.MaxValue))
+      ).map(_.toChar)
+
+      val validCodePoint = notLowSurrogate flatMap { a =>
+        if(a.isHighSurrogate) lowSurrogate map { b => new String(Array(a, b)) }
+        else a.toString
+      }
+
+      Gen.containerOf[List, String](validCodePoint) map (_.mkString)
+    }
+
     val hash = new MurmurHash3(1)
     forAll { (text: String) =>
       hash(text) must equal (hash(text.getBytes("UTF-16BE")))
